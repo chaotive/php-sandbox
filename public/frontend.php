c79db8219360913602ca34d87bfe96c0834161a6
chaotive <leonardomontenegro123@gmail.com>
2021-03-24 23:54:09 +0100

    Consuming service

--- public/frontend.php
+++ public/frontend.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php-sandbox</title>
+</head>
+<body>
 
-echo("hola");
+<h1>Welcome to the PHP sandbox!</h1>
+<div>It is now <?php echo date("Y-m-d H:i:s"); ?></div>
+<div>This is the service content: <?php echo file_get_contents('http://localhost:8080'); ?></div>
+<div>This is the service content, other endpoint: <?php echo file_get_contents('http://localhost:8080/test'); ?></div>
+
+</body>
+</html>
